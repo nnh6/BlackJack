6c97b39c63fb61a52f8cf0646d59a8cf5d10788f
nnh6 <56899420+nnh6@users.noreply.github.com>
2024-04-17 13:16:36 -0400

    Creating the basic JS and HTML files

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+let firstCard = Math.floor(Math.random() * 10)+2;
+let secondCard = Math.floor(Math.random() * 10)+2;
+//console.log(firstCard)
+//console.log(secondCard)
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard + secondCard
+console.log(sum)
+
+//If sum <21 :)
+//If sum == 21 :D
+//If sum > 21 :( lose
